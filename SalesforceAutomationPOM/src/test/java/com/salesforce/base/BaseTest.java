f92f2a9690c3e3372accce850a528922755073f6
gowri <gowri@192.168.0.38>
2022-09-30 13:16:49 -0400

    resources folder update

--- SalesforceAutomationPOM/src/test/java/com/salesforce/base/BaseTest.java
+++ SalesforceAutomationPOM/src/test/java/com/salesforce/base/BaseTest.java
@@ -1,26 +1,17 @@
 package com.salesforce.base;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Method;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.salesforce.utility.CommonUtilities;
-import com.salesforce.utility.Constants;
 import com.salesforce.utility.GenerateReports;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
